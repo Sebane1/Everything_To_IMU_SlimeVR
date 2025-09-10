3f010e54ed4da1e6d8b7e2c93438dce3fb60e166
Sebastina <sebane1@thavea.com>
2025-09-10 15:09:19 -0500

    Update VQF, more crash logging, updated 3ds client
    Update VQF, more crash logging, updated 3ds client

---
+++ PS5-Dualsense-To-IMU-SlimeVR/3DSClient/library/build/templatelib.d
@@ -0,0 +1,2 @@
+templatelib.o: \
+ I:/Slime_Software/PS5-Dualsense-To-IMU-SlimeVR/PS5-Dualsense-To-IMU-SlimeVR/3DSClient/library/source/templatelib.c
--- PS5-Dualsense-To-IMU-SlimeVR/Program.cs
+++ PS5-Dualsense-To-IMU-SlimeVR/Program.cs
@@ -1,29 +1,50 @@
 using AutoUpdaterDotNET;
+using System.Diagnostics;
 
-namespace Everything_To_IMU_SlimeVR {
-    internal static class Program {
+namespace Everything_To_IMU_SlimeVR
+{
+    internal static class Program
+    {
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
+        static void Main()
+        {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
 
 
             bool launchForm = true;
-            AutoUpdater.DownloadPath = Application.StartupPath;
-            AutoUpdater.Synchronous = true;
-            AutoUpdater.Mandatory = true;
-            AutoUpdater.UpdateMode = Mode.ForcedDownload;
-            AutoUpdater.Start("https://raw.githubusercontent.com/Sebane1/Everything_To_IMU_SlimeVR/main/update.xml");
-            AutoUpdater.ApplicationExitEvent += delegate () {
-                launchForm = false;
-            };
+            try
+            {
+                AutoUpdater.DownloadPath = Application.StartupPath;
+                AutoUpdater.Synchronous = true;
+                AutoUpdater.Mandatory = true;
+                AutoUpdater.UpdateMode = Mode.ForcedDownload;
+                AutoUpdater.Start("https://raw.githubusercontent.com/Sebane1/Everything_To_IMU_SlimeVR/main/update.xml");
+                AutoUpdater.ApplicationExitEvent += delegate ()
+                {
+                    launchForm = false;
+                };
 
-            if (launchForm) {
-                ApplicationConfiguration.Initialize();
-                Application.Run(new ConfigurationDisplay());
+            } catch
+            {
+
+            }
+
+            if (launchForm)
+            {
+                try
+                {
+                    ApplicationConfiguration.Initialize();
+                    Application.Run(new ConfigurationDisplay());
+                } catch (Exception ex)
+                {
+                    string crashLog = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "fatalcrash.log");
+                    File.WriteAllText(crashLog, "crashed");
+                    Process.Start("explorer.exe", ex.ToString());
+                }
             }
         }
     }
